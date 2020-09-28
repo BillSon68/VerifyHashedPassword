bc1b947dae3c1e21442e634d707234bd089dfb2d
Billson <Billson68+github@gmail.com>
2020-09-28 13:24:41 +0800

    初次簽入檔案

---
+++ VerifyHashedPassword/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web.Helpers;
+
+namespace VerifyHashedPassword
+{
+    internal class Program
+    {
+        static private string encryptionPassword = "";//加密後的密碼
+
+        private static void Main(string[] args)
+        {
+            string password = "test";
+            //var salt = Crypto.GenerateSalt();
+            //password += salt;
+
+            for (int i = 0; i < 10; i++) {
+                SavePassword(password);
+                Console.WriteLine($"{encryptionPassword} => {CheckPassword(password)}");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void SavePassword(string unhashedPassword)
+        {
+            encryptionPassword = Crypto.HashPassword(unhashedPassword);
+        }
+
+        private static bool CheckPassword(string unhashedPassword)
+        {
+            return Crypto.VerifyHashedPassword(encryptionPassword, unhashedPassword);
+        }
+    }
+}
